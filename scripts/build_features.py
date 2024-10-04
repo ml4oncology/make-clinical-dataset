b35681bdf9dabf61fca98ba16d9a583edba2db84
KevinHeUHN <149023620+KevinHeUHN@users.noreply.github.com>
2024-10-04 11:52:31 -0400

    Kevin/add report (#7)
    * added radiology report preprocess pipeline; added duplicate report EDA
    
    * extract last addendum date; drop duplicate reports
    
    * added clinic notes
    
    * added clinic visits support
    
    * fixed comment
    
    ---------
    
    Co-authored-by: KevinJCHe <kevinjc.he@gmail.com>

--- scripts/build_features.py
+++ scripts/build_features.py
@@ -7,10 +7,12 @@ import os
 import pandas as pd
 
 from make_clinical_dataset.preprocess.cancer_registry import get_demographic_data
+from make_clinical_dataset.preprocess.clinic import get_clinical_notes_data
 from make_clinical_dataset.preprocess.dart import get_symptoms_data
 from make_clinical_dataset.preprocess.emergency import get_emergency_room_data
 from make_clinical_dataset.preprocess.lab import get_lab_data
 from make_clinical_dataset.preprocess.opis import get_treatment_data
+from make_clinical_dataset.preprocess.radiology import get_radiology_data
 from make_clinical_dataset.util import load_included_drugs, load_included_regimens
 
 def parse_args():
@@ -48,6 +50,14 @@ def main():
     # emergency room visits
     er_visit = get_emergency_room_data(data_dir=f'{data_dir}/raw')
     er_visit.to_parquet(f'{data_dir}/interim/emergency_room_visit.parquet.gzip', compression='gzip', index=False)
+
+    # radiology reports
+    reports = get_radiology_data(mrn_map, data_dir=f'{data_dir}/raw')
+    reports.to_parquet(f'{data_dir}/interim/reports.parquet.gzip', compression='gzip', index=False)
+
+    # clinical notes
+    clinical_notes = get_clinical_notes_data(data_dir=f'{data_dir}/raw')
+    clinical_notes.to_parquet(f'{data_dir}/data/interim/clinical_notes.parquet.gzip', compression='gzip', index=False)
     
 if __name__ == '__main__':
     main()
